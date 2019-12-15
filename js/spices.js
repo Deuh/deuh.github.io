b11aa4fe0fbf5ffbcb582d5fdfd952922058b3f8
deuh <a.gerbore@gmail.com>
2019-12-15 21:39:59 +0000

    Add flavours data

--- js/spices.js
+++ js/spices.js
@@ -28,19 +28,19 @@ function renderPeriodicTable(data) {
     for (var key in data) {
         var spice = data[key];
         var grid = 'grid-area: ' + spice.col + ' / ' + spice.row + ';';
-        var el = generateElement(key, spice.id, spice.name, spice.compound_id, grid);
+        var el = generateElement(key, spice.id, spice.name, spice.compound_id, spice.flavours, grid);
         app.appendChild(el);
     }
 }
 
-function generateElement(num, id, name, compound, grid) {
+function generateElement(num, id, name, compound, flavours, grid) {
     var el = document.createElement('div');
     el.setAttribute('class', 'periodic-table__element periodic-table__element--' + compound + ' ' + id);
     el.setAttribute('style', grid);
     el.appendChild(createDiv('name', name));
-    el.appendChild(createDiv('atomic-number', num));
+    el.appendChild(createDiv('atomic-number', compound));
     el.appendChild(createDiv('symbol', id));
-    el.appendChild(createDiv('atomic-weight', compound));
+    el.appendChild(createDiv('atomic-weight', flavours.join('-')));
     return el;
 }
 
